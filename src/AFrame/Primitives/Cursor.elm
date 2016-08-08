59aee4c40861072e72bdb2733f2a6584c50fc763
Eduard Kyvenko <eduard@dinero.dk>
2016-08-09 00:42:15 +0200

    Docs for cursor primitive

--- src/AFrame/Primitives/Cursor.elm
+++ src/AFrame/Primitives/Cursor.elm
@@ -1,31 +1,44 @@
 module AFrame.Primitives.Cursor exposing (..)
 
-{-| -}
+{-| Cursor primitive.
+
+# Definitions
+@docs cursor
+
+# Attributes
+@docs fuse, maxDistance, timeout
+
+-}
 
 import String exposing (toLower)
 import Html exposing (node, Html, Attribute)
 import Html.Attributes exposing (attribute)
 
 
-{-| -}
+{-| The cursor primitive places a reticle or crosshair to add clicking
+    and interactivity with the scene.
+-}
 cursor : List (Attribute msg) -> List (Html msg) -> Html msg
 cursor =
     node "a-cursor"
 
 
-{-| -}
+{-| Whether cursor should also be fuse-based.
+-}
 fuse : Bool -> Attribute msg
 fuse value =
     attribute "fuse" (toLower (toString value))
 
 
-{-| -}
+{-| Maximum distance to check for intersections on entities for clicks.
+-}
 maxDistance : Int -> Attribute msg
 maxDistance value =
     attribute "max-distance" (toString value)
 
 
-{-| -}
+{-| How long to wait (in milliseconds) to trigger a click event if fuse-based.
+-}
 timeout : Int -> Attribute msg
 timeout value =
     attribute "timeout" (toString value)
