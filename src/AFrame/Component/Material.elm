00fe9dfcdbaed6ba5fbf32e44e897f6da4c5ef36
Gabriel Perales <gabrielperales@gmail.com>
2017-07-01 23:30:03 +0200

    added some properties to the material component

--- src/AFrame/Component/Material.elm
+++ src/AFrame/Component/Material.elm
@@ -2,6 +2,8 @@ module AFrame.Component.Material exposing (Property(..), Side(..), material)
 
 import Html exposing (Attribute)
 import Html.Attributes exposing (attribute)
+import Color exposing (Color)
+import Color.Convert exposing (colorToHex)
 
 
 type Side
@@ -11,37 +13,56 @@ type Side
 
 
 type Property
-    = DepthTest Bool
+    = Color Color
+    | DepthTest Bool
     | FlatShading Bool
-    | Opacity Float
-    | Transparent Bool
-    | Side Side
-    | Visible Bool
-    | Offset Float Float
-    | Repeat Float Float
+    | Fog Bool
+    | Height Int
+    | Metalness Float
     | Npot Bool
+    | Offset Float Float
+    | Opacity Float
+    | Repeat Float Float
+    | Roughness Float
+    | Side Side
+    | Src String
+    | Transparent Bool
+    | Visible Bool
+    | Width Int
 
 
 propertyToString : Property -> String
 propertyToString property =
     case property of
+        Color color ->
+            "color: " ++ (colorToHex color)
+
         DepthTest value ->
-            "depthTest: " ++ (toString value)
+            "depthTest: " ++ ((String.toLower << toString) value)
 
         FlatShading value ->
-            "flatShading: " ++ (toString value)
+            "flatShading: " ++ ((String.toLower << toString) value)
+
+        Fog value ->
+            "fog: " ++ ((String.toLower << toString) value)
+
+        Height value ->
+            "height: " ++ (toString value)
 
         Opacity value ->
             "opacity: " ++ (toString value)
 
         Transparent value ->
-            "transparent: " ++ (toString value)
+            "trasparent: " ++ ((String.toLower << toString) value)
+
+        Roughness value ->
+            "roughness: " ++ (toString value)
 
         Side side ->
             "side: " ++ ((String.toLower << toString) side)
 
         Visible value ->
-            "visible: " ++ (toString value)
+            "visible: " ++ ((String.toLower << toString) value)
 
         Offset x y ->
             [ x, y ]
@@ -55,8 +76,17 @@ propertyToString property =
                 |> String.join " "
                 |> flip (++) "repeat: "
 
+        Metalness value ->
+            "metalness: " ++ (toString value)
+
         Npot value ->
-            "npot: " ++ (toString value)
+            "npot: " ++ ((String.toLower << toString) value)
+
+        Src value ->
+            "src: " ++ value
+
+        Width value ->
+            "width: " ++ (toString value)
 
 
 material : List Property -> Attribute msg
