61be53eaad4fc1285573beb5be78392ea52dc4ec
Gabriel Perales <gabrielperales@gmail.com>
2017-07-04 20:20:50 +0200

    added some documentation to controls components

--- src/AFrame/Components/Controls.elm
+++ src/AFrame/Components/Controls.elm
@@ -1,4 +1,14 @@
-module AFrame.Components.Controls exposing (Property(..), Hand, daydreamControls, gearvrControls)
+module AFrame.Components.Controls exposing (Property(..), Hand(..), daydreamControls, gearvrControls, viveControls, oculusTouchControls)
+
+{-| This module provides commponents for defining controllers for Google Daydream, Gear VR, HTC Vive and Oculus Touch
+
+# Components
+@docs daydreamControls, gearvrControls, viveControls, oculusTouchControls
+
+# Types
+@docs Property, Hand
+
+-}
 
 import Html exposing (Attribute)
 import Html.Attributes exposing (attribute)
@@ -6,12 +16,15 @@ import Color exposing (Color)
 import Color.Convert exposing (colorToHex)
 
 
-{-| -}
+{-| Controller's hand
+-}
 type Hand
     = Left
     | Right
 
 
+{-| Controller's properties
+-}
 type Property
     = ArmModel Bool
     | ButtonColor Color
@@ -55,7 +68,8 @@ propertyToString property =
             "rotationOffset: " ++ (toString value)
 
 
-{-| -}
+{-| Deffine a Daydream controller with a list of properties
+-}
 daydreamControls : List Property -> Attribute msg
 daydreamControls properties =
     properties
@@ -64,7 +78,8 @@ daydreamControls properties =
         |> attribute "daydream-controls"
 
 
-{-| -}
+{-| Deffine a Gear VR controller with a list of properties
+-}
 gearvrControls : List Property -> Attribute msg
 gearvrControls properties =
     properties
@@ -73,7 +88,8 @@ gearvrControls properties =
         |> attribute "gearvr-controls"
 
 
-{-| -}
+{-| Deffine a HTC Vive controller with a list of properties
+-}
 viveControls : List Property -> Attribute msg
 viveControls properties =
     properties
@@ -82,7 +98,8 @@ viveControls properties =
         |> attribute "vive-controls"
 
 
-{-| -}
+{-| Deffine a Oculus Touch controller with a list of properties
+-}
 oculusTouchControls : List Property -> Attribute msg
 oculusTouchControls properties =
     properties
